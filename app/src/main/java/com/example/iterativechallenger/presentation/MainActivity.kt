46d30f6f2d200ed209575a53d7c8d9acb1ec47ea
art2202 <arthurrodrigues29@gmail.com>
2020-11-07 16:24:19 -0300

    Botando activity no package certo

--- app/src/main/java/com/example/iterativechallenger/presentation/MainActivity.kt
+++ app/src/main/java/com/example/iterativechallenger/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.iterativechallenger
+package com.example.iterativechallenger.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.iterativechallenger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
