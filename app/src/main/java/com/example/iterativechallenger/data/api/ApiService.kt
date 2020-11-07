72791a70a9a8a5a58d65cda70ed2f8cd050493e1
art2202 <arthurrodrigues29@gmail.com>
2020-11-07 15:58:29 -0300

    criando api service

---
+++ app/src/main/java/com/example/iterativechallenger/data/api/ApiService.kt
@@ -0,0 +1,12 @@
+package com.example.iterativechallenger.data.api
+
+import com.example.iterativechallenger.data.models.ApodDataResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface ApiService {
+
+    @GET
+    suspend fun getApods(@Url url : String) : Response<ApodDataResponse>
+}
