4c9c7191d022f66b8fca70debcd78dd2e5325926
art2202 <arthurrodrigues29@gmail.com>
2020-11-07 16:24:46 -0300

    Criando classe Apod de domain

---
+++ app/src/main/java/com/example/iterativechallenger/domain/entities/Apod.kt
@@ -0,0 +1,13 @@
+package com.example.iterativechallenger.domain.entities
+
+import java.util.*
+
+
+data class Apod(
+    val title : String,
+    val copyright : String,
+    val date : Date,
+    val explanation : String,
+    val url : String,
+    val hdUrl : String
+)
