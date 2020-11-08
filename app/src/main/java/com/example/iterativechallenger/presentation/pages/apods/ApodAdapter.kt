a272b43d46d58410bf3a28317ecad28430bd0283
art2202 <arthurrodrigues29@gmail.com>
2020-11-08 18:43:29 -0300

    Criando adapter para o recycler view

---
+++ app/src/main/java/com/example/iterativechallenger/presentation/pages/apods/ApodAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.iterativechallenger.presentation.pages.apods
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.iterativechallenger.R
+import com.example.iterativechallenger.databinding.ItemApodBinding
+import com.example.iterativechallenger.domain.entities.Apod
+import kotlinx.android.synthetic.main.item_apod.view.*
+
+class ApodAdapter(
+    private val listaApod : List<Apod>,
+    private val onMoreClick :(apod : Apod) -> Unit) : RecyclerView.Adapter<ApodAdapter.ApodViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ApodViewHolder {
+        return ApodViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_apod, parent, false))
+    }
+
+    override fun getItemCount() = listaApod.size
+
+    override fun onBindViewHolder(holder: ApodViewHolder, position: Int) {
+        val binding = holder.binding
+        val apod = listaApod[position]
+        binding?.apod = apod
+        binding?.executePendingBindings()
+
+        Glide.with(holder.itemView.iv_picture.context).load(apod.url).centerCrop().into(holder.itemView.iv_picture)
+
+        holder.itemView.iv_picture.setOnClickListener {
+            onMoreClick(apod)
+        }
+    }
+
+//    @BindingAdapter("getImageFromUrl")
+//    fun bindImage(imgView : ImageView, imageUrl : String){
+//        Glide.with(imgView.context).load(imageUrl).into(imgView)
+//
+//    }
+
+    inner class ApodViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        val binding: ItemApodBinding? = DataBindingUtil.bind(view)
+    }
+}
