fe50b1bb21c6c5cb3e789a486c2d39997b27f464
art2202 <arthurrodrigues29@gmail.com>
2020-11-08 18:44:10 -0300

    Fazendo layout da main

--- app/src/main/java/com/example/iterativechallenger/presentation/pages/apods/MainActivity.kt
+++ app/src/main/java/com/example/iterativechallenger/presentation/pages/apods/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.iterativechallenger.presentation.pages.apods
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.iterativechallenger.R
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.iterativechallenger.core.utils.Response
 import com.example.iterativechallenger.core.utils.Status
+import com.example.iterativechallenger.domain.entities.Apod
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+
+@Suppress("UNCHECKED_CAST")
 class MainActivity : AppCompatActivity() {
 
     private val viewModel : MainViewModel by viewModel()
@@ -34,21 +40,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun showApods(data: Any?) {
 
+        progress_bar.visibility = View.GONE
+        
         if(data is List<*>) {
-            Toast.makeText(this, "deu bom, tamanho: ${data.size}", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "deu bom, tamanho: ${data.size}", Toast.LENGTH_SHORT).show()
+
+            val layoutManager = GridLayoutManager(this, 2)
+            rv_apods.layoutManager = layoutManager
+            rv_apods.adapter = ApodAdapter(data as List<Apod>, ::onMoreClick)
+
         }
 
     }
 
     private fun showLoading(){
 
-        Toast.makeText(this, "Carregando", Toast.LENGTH_SHORT).show()
+        progress_bar.visibility = View.VISIBLE
 
     }
 
     private fun showError(error: Throwable?) {
 
+        progress_bar.visibility = View.GONE
         Toast.makeText(this, "Erro ao baixar informações", Toast.LENGTH_SHORT).show()
 
+    }
+
+    private fun onMoreClick(apod : Apod){
+        Toast.makeText(this, apod.title, Toast.LENGTH_SHORT).show()
     }
 }
