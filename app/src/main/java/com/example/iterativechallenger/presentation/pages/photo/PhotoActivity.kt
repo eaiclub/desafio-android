b587e8750653ba8a4dbfcf50ad225e982e06e07b
art2202 <arthurrodrigues29@gmail.com>
2020-11-09 20:38:58 -0300

    Criando tela para visualizar mais informações

---
+++ app/src/main/java/com/example/iterativechallenger/presentation/pages/photo/PhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.iterativechallenger.presentation.pages.photo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.iterativechallenger.R
+
+class PhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+    }
+}
