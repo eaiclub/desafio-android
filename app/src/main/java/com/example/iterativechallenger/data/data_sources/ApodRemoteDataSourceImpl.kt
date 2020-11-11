f8c5ab6374a9c9300807fee33377385658fa9a24
art2202 <arthurrodrigues29@gmail.com>
2020-11-10 22:49:56 -0300

    melhorando chamada pra api

--- app/src/main/java/com/example/iterativechallenger/data/data_sources/ApodRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/iterativechallenger/data/data_sources/ApodRemoteDataSourceImpl.kt
@@ -10,21 +10,16 @@ import java.util.*
 
 class ApodRemoteDataSourceImpl(private val api : ApiService) : ApodRemoteDataSource {
 
-    override suspend fun getApod(dataInicial : Calendar, dataFinal : Calendar): List<ApodDataResponse> {
+    override suspend fun getApod(dataInicial : String, dataFinal : String): List<ApodDataResponse> {
 
-        val sdf = SimpleDateFormat(Constantes.FORMATO_ANO_MES_DIA, Locale.US)
-
-        val startDate = sdf.format(dataInicial.time)
-        val endDate = sdf.format(dataFinal.time)
-
-        val response = api.getApods("apod?api_key=${BuildConfig.API_KEY}&start_date=$startDate&end_date=$endDate")
+        val response = api.getApods(BuildConfig.API_KEY, dataInicial, dataFinal)
 
         when {
             response.isSuccessful -> {
                 return response.body()!!
             }
             response.code() == 400 -> {
-                throw Throwable("Bad request")
+                throw BadRequestThrowable()
             }
             response.code() == 401 -> {
                 throw InvalidApiKeyThrowable()
