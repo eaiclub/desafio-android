5c0aaa8e3d17da9cd38bfbfc37e1221a2e8122b1
Vitor <nun720@gmail.com>
2020-11-23 14:22:00 -0300

    Adicionada arquivos para estrutura da UI e definição do navigation

---
+++ app/src/main/java/com/example/desafio_android/ui/fragments/ApodListFragment.kt
@@ -0,0 +1,18 @@
+package com.example.desafio_android.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.desafio_android.R
+
+
+class ApodListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_apod_list, container, false)
+    }
+
+}
