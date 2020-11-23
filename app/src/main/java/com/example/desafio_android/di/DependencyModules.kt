ec7cf352bd83da6fe70fceb2a16e95a84c3e4ff5
Vitor <nun720@gmail.com>
2020-11-23 12:58:33 -0300

    Criado arquivos para injeção de dependencias com Koin

--- app/src/main/java/com/example/desafio_android/di/DependencyModules.kt
+++ app/src/main/java/com/example/desafio_android/di/DependencyModules.kt
@@ -1,27 +1,8 @@
 package com.example.desafio_android.di
 
-import com.example.desafio_android.common.ResponseHandler
-import com.example.desafio_android.common.domain.models.NasaApodResponse
-import com.example.desafio_android.common.domain.repository.NasaRepository
-import com.example.desafio_android.data.remote.provideNasaApi
-import com.example.desafio_android.data.remote.provideRetrofit
-import org.koin.core.module.Module
-import org.koin.dsl.module
-
-val apiModule = module {
-    factory { ResponseHandler() }
-    factory { provideRetrofit() }
-    factory { provideNasaApi(get()) }
-}
-lateinit var viewModelModule: Module
-lateinit var useCaseModule: Module
-val repositoryModule = module {
-    factory { NasaRepository(get(), get()) }
-}
-
 val modulesList = listOf(
     apiModule,
-//    viewModelModule,
-//    useCaseModule,
+    viewModelModule,
+    useCaseModule,
     repositoryModule
 )
---
+++ app/src/main/java/com/example/desafio_android/di/useCaseModule.kt
@@ -0,0 +1,5 @@
+package com.example.desafio_android.di
+
+import org.koin.core.module.Module
+
+lateinit var useCaseModule: Module
