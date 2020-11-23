c52e6d36366d18e265e08a08629006e1f637d0cb
Vitor <nun720@gmail.com>
2020-11-23 14:52:37 -0300

    Atualizada MainActivity.kt para utilizar o Jetpack Navigation. ViewModel da activity será compartilhada com o fragmento

--- app/src/main/java/com/example/desafio_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/desafio_android/ui/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.desafio_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.desafio_android.R
+import com.example.desafio_android.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: NasaViewModel by viewModel()
+    private var appBarConfiguration: AppBarConfiguration? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration!!)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration!!)
     }
 }
